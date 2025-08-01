78cf58d3d8f808ec83f47fe0a98ba9d1456e50fc
RaffaelloCompiacente <cmprfl03h16m208e@studenti.unical.it>
2025-08-01 15:51:01 +0200

    Fix

---
+++ src/main/java/model/FermataFerroviaria.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class FermataFerroviaria{
+    private final String station;
+    private final LocalDateTime orarioArrivoFermata;
+    private final LocalDateTime orarioPartenzaFermata;
+
+    public FermataFerroviaria(String stat)
+}
